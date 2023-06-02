70fe85c192c0c5a2930607e1a6418954ceb05ddc
Gabriella Borges <gabimullerborges@gmail.com>
2023-06-02 20:07:19 -0300

    Refactor arquivos js e css

--- js/validacao.js
+++ js/validacao.js
@@ -1,13 +1,5 @@
 
-const validadores = {
-    dataNascimento:input => validaDataNascimento(input),
-    cpf:input => validaCPF(input),
-    cep:input => recuperarCEP(input)
-    }
-
-
-
-function valida(input) {
+export function valida(input) {
 
     const tipoDeInput = input.dataset.tipo /*acessa o input, depois os datas do input e os tipos "tipo" do data*/
 
@@ -16,68 +8,73 @@ function valida(input) {
     }
 
     if(input.validity.valid) {
-        input.parentElement.classList.remove('input-container--invalido')
+        input.parentElement.classList.remove('input-container--invalido');
         input.parentElement.querySelector('.input-mensagem-erro').innerHTML = ''
     } else {
-        input.parentElement.classList.add('input-container--invalido')
+        input.parentElement.classList.add('input-container--invalido'); 
         input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemDeErro(tipoDeInput, input)
     }
 }
 
-
+const validadores = { /* esse objeto fala sobre os tipos de input que sofrerão validação específica*/
+    dataNascimento:input => validaDataNascimento(input), /* chama a função validadora */
+    cpf:input => validaCPF(input),
+    cep:input => recuperarCEP(input)
+    }
+    
 const tiposDeErro = [
-'valueMissing',
-'typeMismatch',
-'patternMismatch',
-'customError'
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
 ]
 
 const mensagensDeErro = {
-nome: {
-    valueMissing: 'O campo de nome não pode estar vazio.'
-},
-email: {
-    valueMissing: 'O campo de email não pode estar vazio.',
-    typeMismatch: 'O email digitado não é válido.'
-},
-senha: {
-    valueMissing: 'O campo de senha não pode estar vazio.',
-    patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
-},
-dataNascimento: {
-    valueMissing: 'O campo de data de nascimento não pode estar vazio.',
-    customError: 'Você deve ser maior que 18 anos para se cadastrar.'
-},
-cpf: {
-    valueMissing: 'O campo de CPF não pode estar vazio.',
-    customError: 'O CPF digitado não é válido.' 
-},
-cep: {
-    valueMissing: 'O campo de CEP não pode estar vazio.',
-    patternMismatch: 'O CEP digitado não é válido.',
-    customError: 'Não foi possível buscar o CEP.'
-},
-logradouro: {
-    valueMissing: 'O campo de logradouro não pode estar vazio.'
-},
-cidade: {
-    valueMissing: 'O campo de cidade não pode estar vazio.'
-},
-estado: {
-    valueMissing: 'O campo de estado não pode estar vazio.'
-},
-preco: {
-    valueMissing: 'O campo de preço não pode estar vazio.'
-}
-}
+        nome: {
+            valueMissing: 'O campo de nome não pode estar vazio.'
+        },
+        email: {
+            valueMissing: 'O campo de email não pode estar vazio.',
+            typeMismatch: 'O email digitado não é válido.'
+        },
+        senha: {
+            valueMissing: 'O campo de senha não pode estar vazio.',
+            patternMismatch: 'A senha deve conter entre 6 a 12 caracteres, deve conter pelo menos uma letra maiúscula, um número e não deve conter símbolos.'
+        },
+        dataNascimento: {
+            valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+            customError: 'Você deve ser maior que 18 anos para se cadastrar.'
+        },
+        cpf: {
+            valueMissing: 'O campo de CPF não pode estar vazio.',
+            customError: 'O CPF digitado não é válido.' 
+        },
+        cep: {
+            valueMissing: 'O campo de CEP não pode estar vazio.',
+            patternMismatch: 'O CEP digitado não é válido.',
+            customError: 'Não foi possível buscar o CEP.'
+        },
+        logradouro: {
+            valueMissing: 'O campo de logradouro não pode estar vazio.'
+        },
+        cidade: {
+            valueMissing: 'O campo de cidade não pode estar vazio.'
+        },
+        estado: {
+            valueMissing: 'O campo de estado não pode estar vazio.'
+        }
+    }
+
 
 
 function mostraMensagemDeErro(tipoDeInput, input) {
-let mensagem = ''
-tiposDeErro.forEach(erro => {
-    if(input.validity[erro]) {
-        mensagem = mensagensDeErro[tipoDeInput][erro]
-    }
+ 
+    let mensagem = '';
+
+    tiposDeErro.forEach(erro => {
+        if(input.validity[erro]) {
+            mensagem = mensagensDeErro[tipoDeInput][erro] /*acessando o objeto e as suas camadas [][]*/
+        }
 })
 
 return mensagem
@@ -103,11 +100,11 @@ function maioridade(dataDeNascimentoAReceber) {
 }
 
 function validaCPF(input) {
-
+    const cpfDigitado = input.value;
     const cpfFormatado = input.value.replace(/\D/g, '') /* \D representa um caractere que não seja um dígito numérico. É equivalente a [^\d].*/
     let mensagem = ''
 
-    if(!checaCPFRepetido(cpfFormatado)) {
+    if(!checaCPFRepetido(cpfFormatado) || /[a-zA-Z]/.test(cpfDigitado)) {
         mensagem = 'O CPF digitado não é válido.'
     }
 
@@ -169,11 +166,12 @@ if(!input.validity.patternMismatch && !input.validity.valueMissing) {
 }
 
 function preencheCamposComCEP(data) {
-const logradouro = document.querySelector('[data-tipo="logradouro"]')
-const cidade = document.querySelector('[data-tipo="cidade"]')
-const estado = document.querySelector('[data-tipo="estado"]')
+    const logradouro = document.querySelector('[data-tipo="logradouro"]')
+    const cidade = document.querySelector('[data-tipo="cidade"]')
+    const estado = document.querySelector('[data-tipo="estado"]')
 
-logradouro.value = data.logradouro
-cidade.value = data.localidade
-estado.value = data.uf
+    logradouro.value = data.logradouro
+    cidade.value = data.localidade
+    estado.value = data.uf
 }
+
